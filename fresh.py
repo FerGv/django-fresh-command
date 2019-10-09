eb3b5391a60f6ffbaa5fbd005b86a37d67c4f471
Fernando Garcia <jfer.garciav@gmail.com>
2019-10-08 20:21:47 -0500

    Updated to run makemigration and migrate commands

--- fresh.py
+++ fresh.py
@@ -1,11 +1,12 @@
 import os
 
+from django.core.management import call_command
 from django.core.management.base import BaseCommand, CommandError
 from django.db import connection
 
 
 class Command(BaseCommand):
-    help = 'Drops all tables and deletes migration files'
+    help = 'Drops all tables, then remakes and migrates migrations'
 
     def add_arguments(self, parser):
         parser.add_argument('basepath', type=str)
@@ -17,11 +18,18 @@ class Command(BaseCommand):
                     DO $$ DECLARE
                         r RECORD;
                     BEGIN
-                        -- if the schema you operate on is not "current", you will want to
-                        -- replace current_schema() in query with 'schematodeletetablesfrom'
+                        -- if the schema you operate on is not "current",
+                        -- you will want to replace current_schema() in
+                        -- query with 'schematodeletetablesfrom'
                         -- *and* update the generate 'DROP...' accordingly.
-                        FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = current_schema()) LOOP
-                            EXECUTE 'DROP TABLE IF EXISTS ' || quote_ident(r.tablename) || ' CASCADE';
+                        FOR r IN (
+                            SELECT tablename
+                            FROM pg_tables
+                            WHERE schemaname = current_schema()
+                        ) LOOP
+                            EXECUTE 'DROP TABLE IF EXISTS ' ||
+                            quote_ident(r.tablename) ||
+                            ' CASCADE';
                         END LOOP;
                     END $$;
                 """, [])
@@ -30,9 +38,14 @@ class Command(BaseCommand):
                 for name in files:
                     if 'migrations' in root and \
                         'apps' in root and \
-                        name != '__init__.py':
+                            name != '__init__.py':
                         os.remove(os.path.join(root, name))
-        except Exception as e:
-            raise CommandError('An error has ocurred while dropping tables. \n %s' % e)
 
-        self.stdout.write(self.style.SUCCESS('Tables have been dropped'))
+            call_command('makemigrations')
+            call_command('migrate')
+        except Exception as e:
+            raise CommandError(
+                f'An error has ocurred while dropping tables. \n{e}'
+            )
+
+        self.stdout.write(self.style.SUCCESS('Database has been refreshed'))
